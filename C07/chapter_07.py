6d27cb25a492563918882cd00fa27793c1be0c69
Quinn <phil@philquinn.co.uk>
2024-12-19 12:55:33 +0000

    C07 and C08

---
+++ C07/chapter_07.py
@@ -0,0 +1,10 @@
+class PolygonModel():
+    def __init__(self, points):
+        self.points = points
+        self.rotation_angle = 0
+        self.x = 0
+        self.y = 0
+
+    def rotate(self, angle):
+        pass
+
